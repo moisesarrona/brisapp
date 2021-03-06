723bcf1da1a690fd01a6bbbbfb0286bffb362c88
Moises Arrona <57106162+MoisesArrona@users.noreply.github.com>
2021-03-05 23:01:02 -0600

    Rutas
    Se crearon las rutas que se usarán e el proyecto

--- routes/web.php
+++ routes/web.php
@@ -1,80 +1,21 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\EmpleyeeController;
+use App\Http\Controllers\PackageController;
+use App\Http\Controllers\PartyController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProviderController;
+use App\Http\Controllers\SalaryController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/empleado', function () {
-    return view('empleado/index');
-});
-
-Route::get('/empleado/mostrar', function () {
-    return view('empleado/show');
-});
-
-
-Route::get('/fiesta', function () {
-    return view('fiesta/index');
-});
-
-Route::get('/fiesta/mostrar', function () {
-    return view('fiesta/show');
-});
-
-
-Route::get('/proveedor', function () {
-    return view('proveedor/index');
-});
-
-Route::get('/proveedor/mostrar', function () {
-    return view('proveedor/show');
-});
-
-
-Route::get('/producto', function () {
-    return view('producto/index');
-});
-
-Route::get('/producto/mostrar', function () {
-    return view('producto/show');
-});
-
-
-Route::get('/cliente', function () {
-    return view('cliente/index');
-});
-
-Route::get('/cliente/mostrar', function () {
-    return view('cliente/show');
-});
-
-
-Route::get('/salario', function () {
-    return view('salario/index');
-});
-
-Route::get('/salario/mostrar', function () {
-    return view('salario/show');
-});
-
-
-Route::get('/paquete', function () {
-    return view('paquete/index');
-});
-
-Route::get('/paquete/mostrar', function () {
-    return view('paquete/show');
+Route::prefix('admin')->group(function () {
+    Route::resource('salary', [SalaryController::class]);
+    Route::resource('employee', [EmpleyeeController::class]);
+    Route::resource('provider', [ProviderController::class]);
+    Route::resource('product', [ProductController::class]);
+    Route::resource('customer', [CustomerController::class]);
+    Route::resource('packege', [PackageController::class]);
+    Route::resource('party', [PartyController::class]);
 });
